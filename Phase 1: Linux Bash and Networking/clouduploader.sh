9fc7a1312c4dc2821a60885f0ae9d6ecb028b596
Aleonogwe Gideon <galosikhena@gmail.com>
2024-06-07 22:41:32 +0100

    changed the name of the folder and added a pipe to the pv command

---
+++ Phase 1: Linux Bash and Networking/clouduploader.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+green_echo() {
+    echo -e "\e[32m$1\e[0m"
+}
+
+red_echo() {
+    echo -e "\e[31m$1\e[0m"
+}
+
+#----------------------
+# Install Pipe Viewer
+#----------------------
+green_echo "Installing Pipe Viewer to monitor data being sent"
+sudo apt install pv
+
+#----------------------
+# Creating an S3 Bucket
+#----------------------
+read -rp 'S3 Bucket Name: ' bucket
+read -rp 'Region: ' region
+green_echo "Creating an S3 bucket..."
+aws s3 mb "s3://$bucket" --region "$region"
+green_echo "S3 bucket created!"
+
+#-----------
+# File Check
+#-----------
+green_echo "Checking file..."
+if [ ! -f "$1" ]; then
+    red_echo "File does not exist."
+    exit 1
+fi
+
+#-----------------------
+# File Upload & Feedback
+#-----------------------
+green_echo "Uploading & getting feedback..."
+file_size=$(stat -c %s "$1")
+if ! pv -p "$1" | aws s3 cp "$1" "s3://$bucket/$(basename "$1")" --storage-class STANDARD --expected-size "$file_size"; then
+    red_echo "Upload failed."
+else
+    green_echo "Upload successful."
+fi
+
+#------------------------------------
+# Sharing Objects with Presigned URLs
+#------------------------------------
+green_echo "Now generating shareable link to upload..."
+aws s3 presign "s3://$bucket/$(basename "$1")" --expires-in 604800 --region "$region" --endpoint-url "https://s3.$region.amazonaws.com"
